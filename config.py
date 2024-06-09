f64efc070f4053afd765d91ed438eaf565d1b251
Harley Franco <hdfranco@usc.edu>
2024-06-09 19:44:29 -0400

    Updated config.py with easier to read instructions

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-TOKEN = 'INSERT TOKEN HERE'
-guildId = 'insert guild id (Server ID) here'
+# Replace "Insert Token" with your bot token
+TOKEN = 'Insert Token'
+# Replace 0000 with your guild/server id
+guildId = 0000
