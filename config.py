24f985b67920e9bc5f866cb5a19471db4758fd45
Harley Franco <hdfranco@usc.edu>
2024-06-09 21:51:55 -0400

    -Added Opus Location and Music Folder Location to config.py

--- config.py
+++ config.py
@@ -2,3 +2,7 @@
 TOKEN = 'Insert Token'
 # Replace 0000 with your guild/server id
 guildId = 0000
+# Replace "Opus Location" with the file path of opus on your computer
+opusLoc = 'Opus Location'
+# Replace "File Path Location" with the file path of the music Folder
+filePath = 'File Path Location'
